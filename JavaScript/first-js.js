9fe72ab68deaddad04f7310c1450469a5bd42cd3
Manoj <manoj@Manoj-MacBook-Air.local>
2024-02-24 19:08:04 +0530

    Basic JavaScript

---
+++ JavaScript/first-js.js
@@ -0,0 +1,44 @@
+var firstName = "Manoj";
+var lastName = "Varma";
+var fullName;
+
+console.log(firstName);
+console.log(lastName);
+console.log("lastName"); // print lastname
+console.log(fullName); // print undefined
+
+var first = 2;
+var second = 4;
+
+
+console.log(first);
+console.log(second);
+
+console.log(typeof(first));
+console.log(typeof(firstName));
+
+console.log(first + second); //print 6
+console.log(firstName + lastName); //print ManojVarma
+console.log(first + lastName);  //print 2Varma
+console.log(fullName + lastName) ; // print ManojVarma
+console.log(first + fullName); // print 2undefined
+
+
+//What is NaN 
+/*
+    + Operator
+      
+      String + String = Concat to a String
+      Number + String = Concat to a String
+      Number + Number = Addition to a Number
+      undefined + String = Concat to a String
+      undefined + Number = NaN(Not a Number)
+
+*/
+
+// This is single line comment.
+
+/* multiple line comment
+ console.log("Today we are learning JavaScript.");
+console.log("Board Infinity");
+ */ 
---
+++ JavaScript/second-js.js
@@ -0,0 +1,11 @@
+var firstName = "Manoj";
+var lastName = "Varma";
+var fullName;
+
+console.log(firstName);
+console.log(lastName);
+console.log(fullName); // undefined
+
+fullName = firstName + " " + lastName; // New value is givin to fullname
+console.log(fullName);
+
